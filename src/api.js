8576bc01495ace47b238f5ab2f8084b8ca198cdd
biankaiwen111 <biankaiwen111@outlook.com>
2021-08-04 07:00:58 -0700

    fdfdf

--- src/api.js
+++ src/api.js
@@ -4,13 +4,32 @@ const serverless = require("serverless-http");
 const app = express();
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json({
-    hello: "hi!"
-  });
+// router.get("/", (req, res) => {
+//   res.json({
+//     hello: "hi!",
+//   });
+// });
+
+// app.use(`/.netlify/functions/api`, router);
+
+app.post("/agreement", (req, res) => {
+  //console.log(req.body["sections"][1]["content"]);
+  //console.log("--------------");
+  console.log(util.inspect(req.body, { showHidden: false, depth: null }));
+  res.json(req.body);
 });
 
-app.use(`/.netlify/functions/api`, router);
+app.get("*", (req, res) => {
+  res.send("Page not found!");
+});
+
+app.post("*", (req, res) => {
+  res.send("incorrect post");
+});
+
+app.listen(3000, () => {
+  console.log("listen on port 3000");
+});
 
 module.exports = app;
 module.exports.handler = serverless(app);
